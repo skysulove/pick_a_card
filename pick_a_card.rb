2d472a08ec7c22cac0766b500718bae09f791d62
glex <a.gnatovsky@mail.ru>
2020-04-13 20:26:46 +0300

    editing the initial commit

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,5 @@
 
-values = %w[2 3 4 5 6 7 8 9 10 J Q K  A]
+values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 suits = %w[Diamonds Hearts Clubs Spades] #Буби, Черви, Крести, Пики
 
 puts "#{values.sample} of #{suits.sample}"
-
